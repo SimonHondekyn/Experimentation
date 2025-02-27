69f8c1802456cc0de6aa54d36522d9a617c73c5c
Simon Hondekyn <simon.hondekyn@ugent.be>
2025-02-27 19:05:10 +0100

    add minrate

--- containers/apache/config/setup.sh
+++ containers/apache/config/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 a2enmod reqtimeout
-echo "RequestReadTimeout header=103" >> /etc/apache2/apache2.conf
+echo "RequestReadTimeout header=103,minrate=1" >> /etc/apache2/apache2.conf
 sed -i "s/^#\?Timeout.*/Timeout 9999/" /etc/apache2/apache2.conf
