45f7b81167120d70b9a90657199dde5b8a46f763
Simon Hondekyn <simon.hondekyn@ugent.be>
2025-02-27 17:07:21 +0100

    slightly change apache configuration

--- containers/apache/config/setup.sh
+++ containers/apache/config/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-# no configuration yet whatsoever
+echo "RequestReadTimeout header=100" >> /etc/apache2/apache2.conf
+sed -i "s/^#\?Timeout.*/Timeout: 9999/" /etc/apache2/apache2.conf
