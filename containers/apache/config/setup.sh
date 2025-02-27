4c31fe3c188a1d14be7a9f66af9bb28191b895c1
Simon Hondekyn <simon.hondekyn@ugent.be>
2025-02-27 17:27:13 +0100

    fix typo

--- containers/apache/config/setup.sh
+++ containers/apache/config/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "RequestReadTimeout header=100" >> /etc/apache2/apache2.conf
-sed -i "s/^#\?Timeout.*/Timeout: 9999/" /etc/apache2/apache2.conf
+sed -i "s/^#\?Timeout.*/Timeout 9999/" /etc/apache2/apache2.conf
