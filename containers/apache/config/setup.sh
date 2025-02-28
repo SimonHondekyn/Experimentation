59817d4487e32f8db7a9e5eed166b0ea19729535
Simon Hondekyn <simon.hondekyn@ugent.be>
2025-02-28 16:49:08 +0100

    add listenbacklog and maxrequestworkers, remove timeouts

--- containers/apache/config/setup.sh
+++ containers/apache/config/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-a2enmod reqtimeout
-echo "RequestReadTimeout header=103,minrate=1" >> /etc/apache2/apache2.conf
-sed -i "s/^#\?Timeout.*/Timeout 9999/" /etc/apache2/apache2.conf
+#a2enmod reqtimeout
+#echo "RequestReadTimeout header=103,minrate=1" >> /etc/apache2/apache2.conf
+#sed -i "s/^#\?Timeout.*/Timeout 9999/" /etc/apache2/apache2.conf
+#sed -i "s/^#\?MaxRequestWorkers*/MaxRequestWorkers  10/" /etc/apache2/mods-available/mpm_event.conf
+echo "ListenBacklog 128" >> /etc/apache2/apache2.conf
+sed -i 's/^\s*MaxRequestWorkers\s*[0-9]\+/MaxRequestWorkers 100/' /etc/apache2/mods-available/mpm_event.conf
