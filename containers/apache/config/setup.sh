0cf53d15f3c7bc5dcccdea0ff51782b7fce28fc5
SimonHondekyn <simonhondekyn@gmail.com>
2025-03-15 20:21:23 +0100

    split up apache container, fix heartbleed and heartleech containers

--- containers/apache/config/setup.sh
+++ containers/apache/config/setup.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
 echo "ListenBacklog 128" >> /etc/apache2/apache2.conf
-sed -i 's/^\s*MaxRequestWorkers\s*[0-9]\+/MaxRequestWorkers 100/' /etc/apache2/mods-available/mpm_event.conf
+sed -i 's/^\(\s*\)MaxRequestWorkers\s*[0-9]\+/\1MaxRequestWorkers 100/' /etc/apache2/mods-available/mpm_event.conf
 
-chown -R www-data:www-data /var/www/html/dv
-chmod -R 755 /var/www/html/dv
-
-service mysql start
-sleep 5
-
-mysql -u root <<EOF
-USE mysql;
-UPDATE user SET plugin='mysql_native_password' WHERE user='root';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-FLUSH PRIVILEGES;
-EXIT;
-EOF
-
-mysql -u root -p'root' < /var/www/mysql_init.sql
-rm -rf /var/www/mysql_init.sql
-
-service mysql stop
+a2dismod mpm_prefork
+a2enmod mpm_event
